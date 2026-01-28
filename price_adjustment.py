72968fca49026e71130fbd2b5c534615dae1a507
Arun Krishna Lama <arun@Aruns-MacBook-Pro.local>
2026-01-28 21:51:46 +0545

    trying to run adjusteder

--- price_adjustment.py
+++ price_adjustment.py
@@ -89,7 +89,7 @@ if all_companies_adjusted:
     all_adj_companies_data.index = all_adj_companies_data.index.strftime('%Y-%m-%d')
     
     # Write to Google Sheet
-    write_to_google_sheet(all_adj_companies_data, adjusted_price_sheet_id, mode='overwrite')
+    # write_to_google_sheet(all_adj_companies_data, adjusted_price_sheet_id, mode='overwrite')
     print(f"✅ Price adjustment completed for {len(active_companies_tickers)} companies!")
 else:
     print("❌ No companies to process!")
